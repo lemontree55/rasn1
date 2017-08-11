f3b43e0857d3b10fc09bc90098bc0540e2950aa1
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-08-11 08:14:04 +0200

    Fix types_spec to works with ruby 2.4

--- spec/types_spec.rb
+++ spec/types_spec.rb
@@ -5,25 +5,27 @@ module RASN1
   describe Types do
     describe '.primitives' do
       it 'gives all subclasses of Primitive' do
-        expect(Types.primitives).to eq([Types::Boolean,
-                                        Types::Integer,
-                                        Types::BitString,
-                                        Types::OctetString,
-                                        Types::Null,
-                                        Types::ObjectId,
-                                        Types::Enumerated,
-                                        Types::Utf8String,
-                                        Types::NumericString,
-                                        Types::PrintableString,
-                                        Types::IA5String,
-                                        Types::VisibleString])
+        expect(Types.primitives).to include(Types::Boolean,
+                                            Types::Integer,
+                                            Types::BitString,
+                                            Types::OctetString,
+                                            Types::Null,
+                                            Types::ObjectId,
+                                            Types::Enumerated,
+                                            Types::Utf8String,
+                                            Types::NumericString,
+                                            Types::PrintableString,
+                                            Types::IA5String,
+                                            Types::VisibleString)
       end
     end
 
     describe '.constructed' do
       it 'gives all subclasses of Constructed' do
-        expect(Types.constructed).to eq([Types::Sequence, Types::SequenceOf,
-                                         Types::Set, Types::SetOf])
+        expect(Types.constructed).to include(Types::Sequence,
+                                              Types::SequenceOf,
+                                              Types::Set,
+                                              Types::SetOf)
       end
     end
   end
