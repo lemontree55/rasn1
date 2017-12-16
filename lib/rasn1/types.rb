544f033caa6c501a5d8da4a096dcf5af0b099d0c
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-12-16 13:30:56 +0100

    Types: cache result of Types.primitives and Types.constructed

--- lib/rasn1/types.rb
+++ lib/rasn1/types.rb
@@ -6,15 +6,15 @@ module RASN1
     # Give all primitive types
     # @return [Array<Types::Primitive>]
     def self.primitives
-      self.constants.map { |c| Types.const_get(c) }.
-          select { |klass| klass < Primitive }
+      @primitives ||= self.constants.map { |c| Types.const_get(c) }.
+                           select { |klass| klass < Primitive }
     end
 
     # Give all constructed types
     # @return [Array<Types::Constructed>]
     def self.constructed
-      self.constants.map { |c| Types.const_get(c) }.
-          select { |klass| klass < Constructed }
+      @constructed ||= self.constants.map { |c| Types.const_get(c) }.
+                            select { |klass| klass < Constructed }
     end
   end
 end
