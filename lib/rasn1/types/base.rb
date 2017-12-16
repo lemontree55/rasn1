2c6fe70766a7c8f3b50988f58d5257c1c4542024
Sylvain Daubert <sylvain.daubert@laposte.net>
2017-12-16 17:14:34 +0100

    Types::Base: add #value_to_der and #der_to_value

--- lib/rasn1/types/base.rb
+++ lib/rasn1/types/base.rb
@@ -238,6 +238,19 @@ module RASN1
 
       private
 
+      def value_to_der
+        case @value
+        when Base
+          @value.to_der
+        else
+          @value.to_s
+        end
+      end
+
+      def der_to_value(der, ber:false)
+        @value = der
+      end
+
       def set_options(options)
         set_class options[:class]
         set_optional options[:optional]
--- lib/rasn1/types/octet_string.rb
+++ lib/rasn1/types/octet_string.rb
@@ -19,21 +19,6 @@ module RASN1
         str << "#{@name} " unless @name.nil?
         str << "#{type}: #{value.inspect}"
       end
-
-      private
-
-      def value_to_der
-        case @value
-        when Base
-          @value.to_der
-        else
-          @value.to_s
-        end
-      end
-
-      def der_to_value(der, ber:false)
-        @value = der
-      end
     end
   end
 end
